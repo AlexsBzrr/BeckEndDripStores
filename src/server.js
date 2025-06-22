fc896c0b2e9c051eb556751fb63b0bb464c6fd04
Alex Bezerra <alexbzrr@outlook.com>
2025-06-22 15:45:50 -0300

    CORS

--- src/server.js
+++ src/server.js
@@ -13,18 +13,20 @@ const port = process.env.PORT || 3300;
 const host = process.env.HOST || "localhost";
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "https://drip-stores-wheat.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://drip-stores-wheat.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(express.json());
 app.use("/v1", router);
